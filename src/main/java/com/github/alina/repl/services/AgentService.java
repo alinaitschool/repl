9b99ba675b290b22c80bb970062d17961b9e8ab9
Alina Gabriela Alina <alinaghetler4@gmail.com>
2024-10-21 16:31:39 +0200

    Add required fixes

--- src/main/java/com/github/alina/repl/services/AgentService.java
+++ src/main/java/com/github/alina/repl/services/AgentService.java
@@ -1,45 +1,14 @@
 package com.github.alina.repl.services;
 
-import com.github.alina.repl.exceptions.ResourceNotFoundException;
 import com.github.alina.repl.models.dtos.AgentDTO;
 import com.github.alina.repl.models.dtos.PropertyDTO;
-import com.github.alina.repl.models.entities.Agent;
-import com.github.alina.repl.models.entities.Property;
-import com.github.alina.repl.repositories.AgentRepository;
-import com.github.alina.repl.repositories.PropertyRepository;
-import org.springframework.stereotype.Service;
 
-@Service
-public class AgentService {
+public interface AgentService {
+    AgentDTO save(AgentDTO agentDTO);
 
-    private final AgentRepository agentRepository;
-    private final PropertyRepository propertyRepository;
+    AgentDTO update(AgentDTO agentDTO);
 
-    public AgentService(AgentRepository agentRepository, PropertyRepository propertyRepository) {
-        this.agentRepository = agentRepository;
-        this.propertyRepository = propertyRepository;
-    }
+    AgentDTO findById(Long id);
 
-    public AgentDTO save(AgentDTO agentDTO) {
-        return AgentDTO.from(agentRepository.save(Agent.from(agentDTO)));
-    }
-
-    public AgentDTO update(AgentDTO agentDTO) {
-        if (!agentRepository.existsById(agentDTO.getId())) {
-            throw new ResourceNotFoundException("Agent with id " + agentDTO.getId() + " not found");
-        }
-        return AgentDTO.from(agentRepository.save(Agent.from(agentDTO)));
-    }
-
-    public AgentDTO findById(Long id) {
-        return AgentDTO.from(agentRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("/agent/" + id)));
-    }
-
-    public PropertyDTO addProperty(Long agentId, PropertyDTO propertyDTO) {
-        Agent agent = agentRepository.findById(agentId).orElseThrow(() -> new ResourceNotFoundException("/agent/" + agentId));
-        Property property = Property.from(propertyDTO);
-        property.setAgent(agent);
-        return PropertyDTO.from(propertyRepository.save(property));
-    }
-
+    PropertyDTO addProperty(Long agentId, PropertyDTO propertyDTO);
 }
---
+++ src/main/java/com/github/alina/repl/services/BuyerServiceImpl.java
@@ -0,0 +1,50 @@
+package com.github.alina.repl.services;
+
+import com.github.alina.repl.exceptions.ResourceNotFoundException;
+import com.github.alina.repl.models.dtos.BuyerDTO;
+import com.github.alina.repl.models.dtos.PropertyDTO;
+import com.github.alina.repl.models.entities.Buyer;
+import com.github.alina.repl.models.entities.Property;
+import com.github.alina.repl.repositories.BuyerRepository;
+import com.github.alina.repl.repositories.PropertyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BuyerServiceImpl implements BuyerService {
+    private final BuyerRepository buyerRepository;
+    private final PropertyRepository propertyRepository;
+
+    public BuyerServiceImpl(BuyerRepository buyerRepository, PropertyRepository propertyRepository) {
+        this.buyerRepository = buyerRepository;
+        this.propertyRepository = propertyRepository;
+    }
+
+    @Override
+    public BuyerDTO save(BuyerDTO buyerDTO) {
+        return BuyerDTO.from(buyerRepository.save(Buyer.from(buyerDTO)));
+    }
+
+    @Override
+    public BuyerDTO update(BuyerDTO buyerDTO) {
+        if (!buyerRepository.existsById(buyerDTO.getId())) {
+            throw new ResourceNotFoundException("Buyer with id " + buyerDTO.getId() + " not found");
+        }
+        return BuyerDTO.from(buyerRepository.save(Buyer.from(buyerDTO)));
+    }
+
+    @Override
+    public BuyerDTO findById(Long id) {
+        return BuyerDTO.from(buyerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Buyer with id " + id + " not found")));
+    }
+
+    @Override
+    public List<PropertyDTO> addPropertyToFavorites(Long id, Long propertyId) {
+        Buyer buyer = buyerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Buyer with id " + propertyId + " not found"));
+        Property property = propertyRepository.findById(propertyId).orElseThrow(() -> new ResourceNotFoundException("Property with id " + propertyId + " not found"));
+        buyer.getFavoriteProperties().add(property);
+        Buyer saved = buyerRepository.save(buyer);
+        return saved.getFavoriteProperties().stream().map(PropertyDTO::from).toList();
+    }
+}
