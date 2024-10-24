5fec48e1ce90c8a6fc97876544774600943c5fff
Alina Gabriela Alina <alinaghetler4@gmail.com>
2024-10-24 23:01:12 +0200

    Add exception handler

--- src/main/java/com/github/alina/repl/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/github/alina/repl/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,5 @@
 package com.github.alina.repl.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource Not Found")
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
